59571250b6d715e3f7cc127b92824ec0c14d299c
sanjaykumar0909 <99422881+sanjaykumar0909@users.noreply.github.com>
2022-12-23 04:57:44 +0530

    createFile, deal_card(), calculate_score()

---
+++ Blackjack.py
@@ -0,0 +1,18 @@
+import random
+
+def deal_card():
+  """Returns a random card from the deck."""
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
+
+
+def calculate_score(cards):
+  """Take a list of cards and return the score calculated from the cards"""
+
+  if sum(cards) == 21 and len(cards) == 2:
+    return 0
+  if 11 in cards and sum(cards) > 21:
+    cards.remove(11)
+    cards.append(1)
+  return sum(cards)
